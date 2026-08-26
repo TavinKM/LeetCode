03bd42a623a083691c16df2d6ea97ea997876f6a
Tavin Mistry <21tkm3@queensu.ca>
2026-08-26 14:21:57 -0400

    Delete my-folder/problems directory

--- my-folder/problems/detect_capital/solution.py
+++
@@ -1,5 +0,0 @@
-class Solution(object):
-    def detectCapitalUse(self, word):
-        return word.isupper() or word.islower() or word.istitle()
-           
-        
--- my-folder/problems/find_all_numbers_disappeared_in_an_array/solution.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def findDisappearedNumbers(self, nums):
-        for n in nums:
-            i = abs(n) - 1
-            nums[i] = -abs(nums[i])
-        return [i + 1 for i in range(len(nums)) if nums[i] > 0]
